05634a0b9b5b034e5a737aeca40bd4023d06dd93
Nico Rehwaldt <git_nikku@nixis.de>
2020-03-30 21:24:04 +0200

    chore(project): bundle with rollup
    Drop microbundle, as it does not support inlining of SVGs.

---
+++ rollup.config.js
@@ -0,0 +1,65 @@
+import { terser } from 'rollup-plugin-terser';
+import { string } from 'rollup-plugin-string';
+
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+import pkg from './package.json';
+
+function pgl(plugins=[]) {
+  return [
+    string({
+      include: '**/*.svg'
+    }),
+    resolve({
+      browser: true
+    }),
+    commonjs(),
+    ...plugins
+  ];
+}
+
+const srcEntry = pkg.source;
+
+const umdDist = pkg['unpkg'];
+
+const umdName = 'BpmnJSSketchy';
+
+export default [
+
+  // browser-friendly UMD build
+  {
+    input: srcEntry,
+    output: {
+      file: umdDist.replace(/\.js$/, '.prod.js'),
+      format: 'umd',
+      name: umdName
+    },
+    plugins: pgl([
+      terser()
+    ])
+  },
+  {
+    input: srcEntry,
+    output: {
+      file: umdDist,
+      format: 'umd',
+      name: umdName
+    },
+    plugins: pgl()
+  },
+  {
+    input: srcEntry,
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    external: [
+      'inherits',
+      'min-dash',
+      'min-dom',
+      'tiny-svg'
+    ],
+    plugins: pgl()
+  }
+];
