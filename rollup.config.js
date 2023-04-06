cfc01b282f4a2c9d6e4d1d8997d85ffdf367f9fc
Nico Rehwaldt <git_nikku@nixis.de>
2023-04-06 11:50:33 +0200

    chore: update rollup*

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
-import { terser } from 'rollup-plugin-terser';
 import { string } from 'rollup-plugin-string';
 
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
 
 import pkg from './package.json';
 
