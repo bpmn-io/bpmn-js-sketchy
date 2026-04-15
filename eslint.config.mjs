5531ec299fc041f44fe8f1ed2d92240be4d5e825
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2026-04-15 15:53:20 +0200

    chore: update to eslint@9

---
+++ eslint.config.mjs
@@ -0,0 +1,55 @@
+import bpmnIoPlugin from 'eslint-plugin-bpmn-io';
+
+const files = {
+  ignored: [
+    'dist'
+  ],
+  build: [
+    '*.js',
+    '*.mjs'
+  ],
+  test: [
+    '**/test/**/*.js'
+  ]
+};
+
+export default [
+  {
+    ignores: files.ignored
+  },
+
+  // build
+  ...bpmnIoPlugin.configs.node.map(config => {
+
+    return {
+      ...config,
+      files: files.build
+    };
+  }),
+
+  // lib + test
+  ...bpmnIoPlugin.configs.browser.map(config => {
+
+    return {
+      ...config,
+      ignores: files.build
+    };
+  }),
+
+  // test
+  ...bpmnIoPlugin.configs.mocha.map(config => {
+
+    return {
+      ...config,
+      files: files.test,
+    };
+  }),
+  {
+    languageOptions: {
+      globals: {
+        sinon: true
+      }
+    },
+    files: files.test
+  }
+];
